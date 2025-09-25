fee5aaeea39fa1c1192cf9fdfcd371e69dbc95b0
Thomas Nguyen <121322450+sam0htngy@users.noreply.github.com>
2025-09-24 18:44:20 -0700

    set up post command to get random video ID for brevi

--- brevi/apps/api/main.py
+++ brevi/apps/api/main.py
@@ -1,9 +1,19 @@
 import uvicorn
-from fastapi import FASTAPI
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import uuid
 
-app = FASTAPI(title="Brevi API")
+app = FastAPI(title="Brevi API")
+
+class VideoRequest(BaseModel):
+    uuid_id: str
 
 @app.get("/health")
 def root():
     return {"Status": "Good"}
+
+@app.post("/api/v1/videos")
+def post_data(request: VideoRequest):
+    new_uuid = uuid.uuid4()
+    return {"videos imported": f"{request.uuid_id}"}
